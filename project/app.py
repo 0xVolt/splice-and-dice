81ce481c7f7d616d6dfaa6e63b2f119ea596d0bb
Desh Iyer <desh.iyer@gmail.com>
2023-04-28 16:14:26 +0530

    Update Streamlit app

--- project/app.py
+++ project/app.py
@@ -23,31 +23,40 @@ def extract_text_from_pdf(file_path):
 choice = st.sidebar.selectbox("Select your choice", ["Summarize Text", "Summarize Document"])
 
 if choice == "Summarize Text":
-    st.subheader("Summarize Text using txtai")
+    st.subheader("Summarize Text using T5")
+    
     input_text = st.text_area("Enter your text here")
+    
     if input_text is not None:
         if st.button("Summarize Text"):
             col1, col2 = st.columns([1,1])
+            
             with col1:
                 st.markdown("**Your Input Text**")
                 st.info(input_text)
+            
             with col2:
                 st.markdown("**Summary Result**")
                 result = text_summary(input_text)
                 st.success(result)
 elif choice == "Summarize Document":
-    st.subheader("Summarize Document using txtai")
+    st.subheader("Summarize Document using T5")
+    
     input_file = st.file_uploader("Upload your document here", type=['pdf'])
+    
     if input_file is not None:
         if st.button("Summarize Document"):
             with open("doc_file.pdf", "wb") as f:
                 f.write(input_file.getbuffer())
+                
             col1, col2 = st.columns([1,1])
+            
             with col1:
                 st.info("File uploaded successfully")
                 extracted_text = extract_text_from_pdf("doc_file.pdf")
                 st.markdown("**Extracted Text is Below:**")
                 st.info(extracted_text)
+            
             with col2:
                 st.markdown("**Summary Result**")
                 text = extract_text_from_pdf("doc_file.pdf")
